da863de605e06f986dbfec10a11a566b7de1cef5
Alejandro A <aalvarezbo@unal.edu.co>
2024-03-30 13:40:59 -0500

    feat: add functions to the buttons

--- san-valentin/script.js
+++ san-valentin/script.js
@@ -3,12 +3,12 @@ const buttons = document.querySelector('#buttons');
 const noBtn = document.querySelector('.no');
 const yesBtn = document.querySelector('.yes');
 
-noBtn.addEventListener('click', () => {
-    //RANDOM POSITIONIN OF THE noBTn
-    //HINT # 1: window has properties of the width and height of the screen
-    //HINT # 2: offsetWidth and offsetHeight are properties of html elements
-    //HINT # 3: This button has position 'absolute', which means that its position in the html document could
-    //be given by the style properties left and top (in px values)
+noBtn.addEventListener('mouseover', () => {
+    const width = window.innerWidth - noBtn.offsetWidth;
+    const height = window.innerHeight - noBtn.offsetHeight;
+
+    noBtn.style.top = Math.floor(Math.random() * height) + 'px';
+    noBtn.style.left = Math.floor(Math.random() * width) + 'px';
 });
 
 yesBtn.addEventListener('click', () => {
